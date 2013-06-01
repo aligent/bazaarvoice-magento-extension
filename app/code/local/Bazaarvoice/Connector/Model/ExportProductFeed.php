bad202df4d0a2980e1e499d4cc3afc2b281b4467
Garth Brantley <garth@storefrontconsulting.com>
2013-06-01 10:20:33 -0400

    Cleanup double quotes in product export code.

--- app/code/local/Bazaarvoice/Connector/Model/ExportProductFeed.php
+++ app/code/local/Bazaarvoice/Connector/Model/ExportProductFeed.php
@@ -63,28 +63,28 @@ class Bazaarvoice_Connector_Model_ExportProductFeed extends Mage_Core_Model_Abst
     public function exportDailyProductFeed()
     {
         // Log
-        Mage::log("Start Bazaarvoice product feed generation");
+        Mage::log('Start Bazaarvoice product feed generation');
         // Iterate through all stores / views in this instance
         // (Not the 'admin' store view, which represents admin panel)
         $stores = Mage::app()->getStores(false);
         foreach ($stores as $store) {
             try {
                 if (Mage::getStoreConfig('bazaarvoice/ProductFeed/EnableProductFeed', $store->getId()) === '1') {
-                    Mage::log("    BV - Exporting product feed for store: " . $store->getCode(), Zend_Log::INFO);
+                    Mage::log('    BV - Exporting product feed for store: ' . $store->getCode(), Zend_Log::INFO);
                     $this->exportDailyProductFeedForStore($store);
                 }
                 else {
-                    Mage::log("    BV - Product feed disabled for store: " . $store->getCode(), Zend_Log::INFO);
+                    Mage::log('    BV - Product feed disabled for store: ' . $store->getCode(), Zend_Log::INFO);
                 }
             } catch (Exception $e) {
-                Mage::log("    BV - Failed to export daily product feed for store: " . $store->getCode(), Zend_Log::ERR);
-                Mage::log("    BV - Error message: " . $e->getMessage(), Zend_Log::ERR);
+                Mage::log('    BV - Failed to export daily product feed for store: ' . $store->getCode(), Zend_Log::ERR);
+                Mage::log('    BV - Error message: ' . $e->getMessage(), Zend_Log::ERR);
                 Mage::logException($e);
                 // Continue processing other stores
             }
         }
         // Log
-        Mage::log("End Bazaarvoice product feed generation");
+        Mage::log('End Bazaarvoice product feed generation');
     }
 
     /**
@@ -96,9 +96,9 @@ class Bazaarvoice_Connector_Model_ExportProductFeed extends Mage_Core_Model_Abst
      */
     public function exportDailyProductFeedForStore($store)
     {
-        if (Mage::getStoreConfig("bazaarvoice/ProductFeed/EnableProductFeed", $store->getId()) === "1") {
+        if (Mage::getStoreConfig('bazaarvoice/ProductFeed/EnableProductFeed', $store->getId()) === '1') {
             
-            $productFeedFilePath = Mage::getBaseDir("var") . DS . 'export' . DS . 'bvfeeds';
+            $productFeedFilePath = Mage::getBaseDir('var') . DS . 'export' . DS . 'bvfeeds';
             $productFeedFileName = 'productFeed-' . $store->getCode() . '-' . date('U') . '.xml';
 
             $ioObject = new Varien_Io_File();
@@ -120,25 +120,25 @@ class Bazaarvoice_Connector_Model_ExportProductFeed extends Mage_Core_Model_Abst
                     "  extractDate=\"".date('Y-m-d')."T".date('H:i:s').".000000\">\n");
 
 
-                Mage::log("    BV - processing all categories");
+                Mage::log('    BV - processing all categories');
                 $this->processCategories($ioObject, $store);
-                Mage::log("    BV - completed categories, beginning products");
+                Mage::log('    BV - completed categories, beginning products');
                 $this->processProducts($ioObject, $store);
-                Mage::log("    BV - completed processing all products");
+                Mage::log('    BV - completed processing all products');
 
                 $ioObject->streamWrite("</Feed>\n");
                 $ioObject->streamClose();
 
                 $destinationFile = 
-                    "/" . Mage::getStoreConfig("bazaarvoice/ProductFeed/ExportPath", $store->getId()) . 
-                    "/" . Mage::getStoreConfig("bazaarvoice/ProductFeed/ExportFileName", $store->getId());
+                    '/' . Mage::getStoreConfig('bazaarvoice/ProductFeed/ExportPath', $store->getId()) . 
+                    '/' . Mage::getStoreConfig('bazaarvoice/ProductFeed/ExportFileName', $store->getId());
                 $sourceFile = $productFeedFilePath . DS . $productFeedFileName;
                 $upload = Mage::helper('bazaarvoice')->uploadFile($sourceFile, $destinationFile, $store);
 
                 if (!$upload) {
-                    Mage::log("    Bazaarvoice FTP upload failed! [filename = " . $productFeedFileName . "]");
+                    Mage::log('    Bazaarvoice FTP upload failed! [filename = ' . $productFeedFileName . ']');
                 } else {
-                    Mage::log("    Bazaarvoice FTP upload success! [filename = " . $productFeedFileName . "]");
+                    Mage::log('    Bazaarvoice FTP upload success! [filename = ' . $productFeedFileName . ']');
                     $ioObject->rm($productFeedFileName);
                 }
             }
@@ -168,19 +168,19 @@ class Bazaarvoice_Connector_Model_ExportProductFeed extends Mage_Core_Model_Abst
             // Load category object
             $category = $categoryModel->load($categoryId->getId());
             $categoryExternalId = Mage::helper('bazaarvoice')->getCategoryId($category);
-            $categoryName = htmlspecialchars($category->getName(), ENT_QUOTES, "UTF-8");
-            $categoryPageUrl = htmlspecialchars($category->getCategoryIdUrl(), ENT_QUOTES, "UTF-8");
+            $categoryName = htmlspecialchars($category->getName(), ENT_QUOTES, 'UTF-8');
+            $categoryPageUrl = htmlspecialchars($category->getCategoryIdUrl(), ENT_QUOTES, 'UTF-8');
 
             if (!$category->getIsActive() || empty($categoryExternalId) || is_null($categoryExternalId) || $category->getLevel() == 1) {
-                Mage::log("        BV - Skipping category: " . $category->getUrlKey());
+                Mage::log('        BV - Skipping category: ' . $category->getUrlKey());
                 continue;
             }
 
-            $parentExtId = "";
+            $parentExtId = '';
             $parentCategory = Mage::getModel('catalog/category')->load($categoryId->getParentId());
             // If parent category is the root category, then ignore it
             if (!is_null($parentCategory) && $parentCategory->getLevel() != 1) {
-                $parentExtId = "    <ParentExternalId>" . Mage::helper('bazaarvoice')->getCategoryId($parentCategory) . "</ParentExternalId>\n";
+                $parentExtId = '    <ParentExternalId>' . Mage::helper('bazaarvoice')->getCategoryId($parentCategory) . "</ParentExternalId>\n";
             }
             
             array_push($this->_categoryIdList, $categoryExternalId);
@@ -225,42 +225,42 @@ class Bazaarvoice_Connector_Model_ExportProductFeed extends Mage_Core_Model_Abst
             // Load product object
             $product->load($productId->getId());
             $productExternalId = Mage::helper('bazaarvoice')->getProductId($product);
-            $brand = htmlspecialchars($product->getAttributeText("manufacturer"));
+            $brand = htmlspecialchars($product->getAttributeText('manufacturer'));
 
 
             // A status of 1 means enabled, 0 means disabled
             if ($product->getStatus() != 1 || empty($productExternalId) || is_null($productExternalId)) {
-                Mage::log("        BV - Skipping product: " . $product->getSku());
+                Mage::log('        BV - Skipping product: ' . $product->getSku());
                 continue;
             }
 
 
             $ioObject->streamWrite("<Product>\n".
-                "    <ExternalId>".$productExternalId."</ExternalId>\n".
-                "    <Name>".htmlspecialchars($product->getName(), ENT_QUOTES, "UTF-8")."</Name>\n".
-                "    <Description>".htmlspecialchars($product->getShortDescription(), ENT_QUOTES, "UTF-8")."</Description>\n");
+                '    <ExternalId>'.$productExternalId."</ExternalId>\n".
+                '    <Name>'.htmlspecialchars($product->getName(), ENT_QUOTES, 'UTF-8')."</Name>\n".
+                '    <Description>'.htmlspecialchars($product->getShortDescription(), ENT_QUOTES, 'UTF-8')."</Description>\n");
 
             if (!is_null($brand) && !empty($brand)) {
-                $ioObject->streamWrite("    <Brand><ExternalId>" . $brand . "</ExternalId></Brand>\n");
+                $ioObject->streamWrite('    <Brand><ExternalId>' . $brand . "</ExternalId></Brand>\n");
             }
                 
             /* Make sure that CategoryExternalId is one written to Category section */
             $parentCategories = $productId->getCategoryIds();
             if (!is_null($parentCategories) && count($parentCategories) > 0) {
                 foreach ($parentCategories as $parentCategoryId) {
-                    $parentCategory = Mage::getModel("catalog/category")->load($parentCategoryId);
+                    $parentCategory = Mage::getModel('catalog/category')->load($parentCategoryId);
                     if ($parentCategory != null) {
                         $categoryExternalId = Mage::helper('bazaarvoice')->getCategoryId($parentCategory);
                         if (in_array($categoryExternalId, $this->_categoryIdList)) {
-                            $ioObject->streamWrite("    <CategoryExternalId>" . $categoryExternalId . "</CategoryExternalId>\n");
+                            $ioObject->streamWrite('    <CategoryExternalId>' . $categoryExternalId . "</CategoryExternalId>\n");
                             break;
                         }
                     }
                 }                
             }
             
-            $ioObject->streamWrite("    <ProductPageUrl>".$product->getProductUrl()."</ProductPageUrl>\n".
-                "    <ImageUrl>".$product->getImageUrl()."</ImageUrl>\n".
+            $ioObject->streamWrite('    <ProductPageUrl>'.$product->getProductUrl()."</ProductPageUrl>\n".
+                '    <ImageUrl>'.$product->getImageUrl()."</ImageUrl>\n".
                 "</Product>\n");
         }
         if (count($productIds) > 0) {
