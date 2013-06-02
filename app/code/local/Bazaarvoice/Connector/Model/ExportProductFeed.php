4e457e46fcf4cc90b31c7453e950c33a73ff496e
Garth Brantley <garth@storefrontconsulting.com>
2013-06-02 17:32:59 -0400

    [#50890609] Reenabled FTP upload.

--- app/code/local/Bazaarvoice/Connector/Model/ExportProductFeed.php
+++ app/code/local/Bazaarvoice/Connector/Model/ExportProductFeed.php
@@ -141,8 +141,7 @@ class Bazaarvoice_Connector_Model_ExportProductFeed extends Mage_Core_Model_Abst
                     '/' . Mage::getStoreConfig('bazaarvoice/ProductFeed/ExportPath', $group->getDefaultStoreId()) . 
                     '/' . Mage::getStoreConfig('bazaarvoice/ProductFeed/ExportFileName', $group->getDefaultStoreId());
                 $sourceFile = $productFeedFilePath . DS . $productFeedFileName;
-                //$upload = Mage::helper('bazaarvoice')->uploadFile($sourceFile, $destinationFile, $store);
-                $upload = false;
+                $upload = Mage::helper('bazaarvoice')->uploadFile($sourceFile, $destinationFile, $store);
 
                 if (!$upload) {
                     Mage::log('    Bazaarvoice FTP upload failed! [filename = ' . $productFeedFileName . ']');
