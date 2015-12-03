506b1dec1f5dddba0a8a3c9c44b43eeeb9eae4b5
Dennis Rogers <dennis@storefrontconsulting.com>
2015-12-03 12:21:42 -0500

    BVEXT-132 #comment Added flag to prevent double-encoding when product/category values are encoded for feed.

--- app/code/local/Bazaarvoice/Connector/Model/ProductFeed/Product.php
+++ app/code/local/Bazaarvoice/Connector/Model/ProductFeed/Product.php
@@ -290,8 +290,8 @@ class Bazaarvoice_Connector_Model_ProductFeed_Product extends Mage_Core_Model_Ab
 
         $ioObject->streamWrite("<Product>\n" .
             '    <ExternalId>' . $productExternalId . "</ExternalId>\n" .
-            '    <Name><![CDATA[' . htmlspecialchars($productDefault->getName(), ENT_QUOTES, 'UTF-8') . "]]></Name>\n" .
-            '    <Description><![CDATA[' . htmlspecialchars($productDefault->getData('short_description'), ENT_QUOTES, 'UTF-8') .
+            '    <Name><![CDATA[' . htmlspecialchars($productDefault->getName(), ENT_QUOTES, 'UTF-8', false) . "]]></Name>\n" .
+            '    <Description><![CDATA[' . htmlspecialchars($productDefault->getData('short_description'), ENT_QUOTES, 'UTF-8', false) .
             "]]></Description>\n");
 
         $brandId = $productDefault->getData('brand');
@@ -336,14 +336,14 @@ class Bazaarvoice_Connector_Model_ProductFeed_Product extends Mage_Core_Model_Ab
         $ioObject->streamWrite("    <Names>\n");
         foreach ($productsByLocale as $curLocale => $curProduct) {
             $ioObject->streamWrite('        <Name locale="' . $curLocale . '"><![CDATA[' .
-                htmlspecialchars($curProduct->getData('name'), ENT_QUOTES, 'UTF-8') . "]]></Name>\n");
+                htmlspecialchars($curProduct->getData('name'), ENT_QUOTES, 'UTF-8', false) . "]]></Name>\n");
         }
         $ioObject->streamWrite("    </Names>\n");
         // Write out localized <Descriptions>
         $ioObject->streamWrite("    <Descriptions>\n");
         foreach ($productsByLocale as $curLocale => $curProduct) {
             $ioObject->streamWrite('         <Description locale="' . $curLocale . '"><![CDATA[' .
-                htmlspecialchars($curProduct->getData('short_description'), ENT_QUOTES, 'UTF-8') . "]]></Description>\n");
+                htmlspecialchars($curProduct->getData('short_description'), ENT_QUOTES, 'UTF-8', false) . "]]></Description>\n");
         }
         $ioObject->streamWrite("    </Descriptions>\n");
         // Write out localized <ProductPageUrls>
